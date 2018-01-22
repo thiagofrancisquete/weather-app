067baa4f738c599fc3813dc3176a4777e12f5424
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-01-22 01:44:18 -0200

    import Form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Titles from "./components/Titles"
+import Form from "./components/Form"
 
 class App extends React.Component {
     render(){
         return (
             <div>
                 <Titles />
+                <Form />
             </div>
         );
     }
