60eecf2bbbe88dbd2a35dc5f7b4ac04cb7f39ec9
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-01-23 17:25:55 -0200

    create max and min camps

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Weather = props => (
         <div>
-            { props.city &&  props.country && <p>Location: { props.city } , { props.country }</p> }
-            { props.temperature && <p>Temperature: { props.temperature } ºC</p> }
+            { props.city &&  props.country && <p>Location: { props.city }, { props.country }</p> }
+            { props.temperature && <p>Temperature: { props.temperature } ºC </p> }
+            { props.max && <p>Max: {props.max} ºC </p>}
+            { props.min  && <p>Min: {props.min} ºC </p>}
             { props.humidity && <p>Humidity: { props.humidity } %</p> }
             { props.description && <p>Description: { props.description }</p> }
             { props.error && <p> { props.error } </p> }
